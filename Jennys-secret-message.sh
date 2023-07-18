2e971fad4dced5b8dcdad26c5592b18126f8528a
marinicheva <alin4ikmarin4ik@gmail.com>
2023-07-18 12:32:03 +0200

    added new codewars challang

---
+++ Jennys-secret-message.sh
@@ -0,0 +1,15 @@
+//Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, and would like to greet him slightly different. She added a special case to her function, but she made a mistake.
+
+//Can you help her?
+
+//https://www.codewars.com/kata/55225023e1be1ec8bc000390/train/shell/64b59ef20513b3006401b35a
+
+
+
+#!/bin/bash
+
+if [ "$1" = "Johnny" ]; then
+  echo "Hello. my Love!"
+  else 
+  echo "Hello, $1!"
+fi
