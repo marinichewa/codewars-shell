b1cc6d97792b0916c05e3032b423552057d53116
marinicheva <alin4ikmarin4ik@gmail.com>
2023-07-18 15:25:44 +0200

    added new codewars challang

---
+++ You-Cant-Code-Under-Pressure.sh
@@ -0,0 +1,6 @@
+//Code as fast as you can! You need to double the integer and return it.
+
+//https://www.codewars.com/kata/53ee5429ba190077850011d4/train/shell
+
+# $1 will be an integer. Double it and print it.
+echo "$1*2" | bc
