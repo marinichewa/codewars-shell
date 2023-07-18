8067237520e01f13c0aad303c007eb7edb10da9c
marinicheva <alin4ikmarin4ik@gmail.com>
2023-07-18 14:11:24 +0200

    added new codewars challang

---
+++ Returning-Strings.sh
@@ -0,0 +1,11 @@
+//Make a function that will return a greeting statement that uses an input; your program should return, "Hello, <name> how are you doing today?".
+
+//[Make sure you type the exact thing I wrote or the program may not execute properly]
+
+//codewars.com/kata/55a70521798b14d4750000a4/train/shell
+
+#!/bin/bash
+#your code here
+name=$1
+
+echo "Hello, $1 how are you doing today?"
