d9a8f37c4d9e47bfe73a8d60779ff509482b125f
marinicheva <alin4ikmarin4ik@gmail.com>
2023-07-18 12:50:50 +0200

    added new codewars challang

---
+++ Multiply.sh
@@ -0,0 +1,7 @@
+// This code does not execute properly. Try to figure out why.
+//https://www.codewars.com/kata/50654ddff44f800200000004/train/shell/64b595515cce081b757257c5
+
+#!/bin/bash -e
+a=$1
+b=$2
+echo $((a*b))
