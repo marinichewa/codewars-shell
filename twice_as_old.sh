53b9b4245e3e6d5171cb3e04e415cfee0aee589c
marinicheva <alin4ikmarin4ik@gmail.com>
2023-07-20 10:22:49 +0200

    edit

--- twice_as_old.sh
+++ twice_as_old.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
+
 dad_years_old=$1
 son_years_old=$2
-counter=0
 
-# shellcheck disable=SC1073
-while (( son_years_old*2=dad_years_old=$1))
-        do
-        ((counter++))
-        ((dad_years_old++))
-        ((son_years_old++))
-        done
-
-echo "$counter"
-
-exit
+(( res=dad_years_old-2*son_years_old ))
+echo ${res#-}
